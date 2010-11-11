82382392f74ae9e6da2aa8e571e0f04e4ebc6eac
Matt Stine <matt.stine@gmail.com>
2010-11-11 13:05:43 -0600

    Add first Se test for Fluffbox

--- src/test/java/com/deepsouthsoftware/seworkshop/BingTest.java
+++ src/test/java/com/deepsouthsoftware/seworkshop/BingTest.java
@@ -12,7 +12,7 @@ public class BingTest {
 	
 	@Before
 	public void setUp() throws Exception {
-		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://www.bing.com/");
+		selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://www.bing.com/");
 		selenium.start();
 	}
 
